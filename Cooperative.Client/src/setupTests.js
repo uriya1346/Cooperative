6abddbc1159d34c32c031a576917bbfdc5cb7c1a
shimon driham <95618903+shimondriham@users.noreply.github.com>
2022-07-13 17:06:46 +0300

    bb

--- Cooperative.Client/src/setupTests.js
+++ Cooperative.Client/src/setupTests.js
@@ -6,15 +6,3 @@ import '@testing-library/jest-dom';
 //ferfref
 //ffffs
 //8888888
-// test5
-<<<<<<< HEAD
-// test6
-<<<<<<< HEAD
-=======
-// test6
-// test666
->>>>>>> test5
-=======
-// sssssssssssssss
-// sssssssssssssss
->>>>>>> test5
